58bb1761077c846034ffb18e252038a1a5958922
sou <hirawatasou@gmail.com>
2022-06-20 16:18:39 +0900

    ~002

---
+++ 001.py
@@ -0,0 +1,8 @@
+# coding UTF8
+
+apple = 5  # りんごの個数
+N = int(input())  # みかんの個数
+
+total = apple + N  # 合計個数を計算
+
+print(total)  # 出力
---
+++ 002.py
@@ -0,0 +1,3 @@
+# coding UTF8
+a1, a2, a3 = map(int, input().split()) #値を入力
+print(a1 + a2 + a3) #足して出力
