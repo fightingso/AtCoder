ef8c3827087e335ea941e0c55c5f2eeb7684feec
SouHirawata <hirawatasou@gmail.com>
2022-07-09 22:56:16 +0900

    [update]C

--- ABC259/C.py
+++ ABC259/C.py
@@ -5,7 +5,7 @@ T = list(str(input()))
 def ins(S,T):
     for i in range(len(S)):
         if S[i] != T[i]:
-            if S[i - 2] == T[i]:
+            if S[i - 1] == T[i]:
                 if S[i - 2] == S[i - 1]:
                     S.insert(i - 2, S[i-2])
     if len(S) != len(T):
