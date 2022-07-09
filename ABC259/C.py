907abc9aa3555afe93974f755fa272848d055610
SouHirawata <hirawatasou@gmail.com>
2022-07-10 02:44:29 +0900

    ABC259-C

--- ABC259/C.py
+++ ABC259/C.py
@@ -1,20 +1,37 @@
 # -*- coding: utf-8 -*-
+from itertools import groupby
+
 S = list(str(input()))
 T = list(str(input()))
 
-def ins(S,T):
-    for i in range(len(S)):
-        if S[i] != T[i]:
-            if S[i - 1] == T[i]:
-                if S[i - 2] == S[i - 1]:
-                    S.insert(i - 2, S[i-2])
-    if len(S) != len(T):
-        return ins(S,T)
-    else:
-        return S
+def rle(s):
+    tmp, count, ans = s[0], 1, ""
+    for i in range(1, len(s)):
+        if tmp == s[i]:
+            count += 1
+        else:
+            ans += tmp+str(count)
+            tmp = s[i]
+            count = 1
+    ans += tmp+str(count)
+    return ans
 
-S = ins(S,T)
-if S == T:
-    print('Yes')
-else:
+def no():
     print('No')
+    exit()
+
+S,T = rle(S),rle(T)
+if len(S) != len(T):
+    no()
+
+S,T = iter(S),iter(T)
+for s1,s2,t1,t2 in zip(S,S,T,T):
+    if s1 != t1:
+        no()
+    elif int(t2) == 1 and int(s2) != 1:
+        no()
+    elif int(t2) > 1 and int(s2) > int(t2):
+        no()
+    elif int(t2) > 1 and int(s2) == 1:
+        no()
+print('Yes')
