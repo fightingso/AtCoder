740686af84020a48e5391514957bcd17bef4aceb
SouHirawata <hirawatasou@gmail.com>
2023-01-07 00:33:16 +0900

    :gem: abca

---
+++ abc/abc278/a/main.cpp
@@ -0,0 +1,55 @@
+#ifndef HIDDEN_IN_VS_CODE
+
+// Load the library
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortcut types
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vvvl = vector<vvl>;
+using vd = vector<double>;
+using vvd = vector<vd>;
+using vvvd = vector<vvd>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vvvc = vector<vvc>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vvvb = vector<vvb>;
+
+// Define generic macros
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
+#define rrep(i, n) for (int i = (int)(n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (int)(n); i > 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define len() size()
+#define print(x) cout << x << endl
+#define exit(s)        \
+    cout << s << endl; \
+    exit(0);
+
+#endif
+
+int N, K;
+vi A(1 << 10, 0);
+
+int main()
+{
+    cin >> N >> K;
+    rep(i, N) cin >> A[i];
+
+    for (int i = 0 + K; i < N + K; i ++) {
+        if (i == N + K - 1) cout << A[i] << endl;
+        else cout << A[i] << " ";
+    }
+}
