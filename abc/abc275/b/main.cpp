bc909c30dfcdcdc974ff14d5825ee14381cd65c0
SouHirawata <hirawatasou@gmail.com>
2023-01-07 20:30:25 +0900

    :gem: abcb

---
+++ abc/abc275/b/main.cpp
@@ -0,0 +1,56 @@
+#ifndef HIDDEN_IN_VS_CODE
+
+// Load the library
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortcut types
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vvvl = vector<vvl>;
+using vd = vector<double>;
+using vvd = vector<vd>;
+using vvvd = vector<vvd>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vvvc = vector<vvc>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vvvb = vector<vvb>;
+
+// Define generic macros
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
+#define rrep(i, n) for (int i = (int)(n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (int)(n); i > 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define len() size()
+#define print(x) cout << x << endl
+#define exit(s)        \
+    cout << s << endl; \
+    exit(0);
+
+#endif
+
+ll A, B, C, D, E, F;
+ll mod = 998244353;
+
+int main()
+{
+  cin >> A >> B >> C >> D >> E >> F;
+  ll x, y;
+  x = ((A%mod)*(B%mod))%mod;
+  x = ((x%mod)*(C%mod))%mod;
+  y = ((D%mod)*(E%mod))%mod;
+  y = ((y%mod)*(F%mod))%mod;
+  ll ans = (x+mod-y) % mod;
+  print(ans);
+}
