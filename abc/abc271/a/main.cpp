2e9720d942fc722c4a17939cd22f67d89ce83990
SouHirawata <hirawatasou@gmail.com>
2023-01-28 01:57:06 +0900

    :gem: abca

---
+++ abc/abc271/a/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long N;
+
+int main() {
+  cin >> N;
+  cout << uppercase;
+  cout << setw(2);
+  cout << setfill('0');
+  cout << hex << N << endl;
+  return 0;
+}
