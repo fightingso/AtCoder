cf839ce5bbb6b50926d5f81767a02ced244f8881
SouHirawata <hirawatasou@gmail.com>
2023-03-05 14:16:00 +0900

    :gem: dp_a

--- dp/lap2/a/main.cpp
+++ dp/lap2/a/main.cpp
@@ -5,19 +5,15 @@ using ll=long long;
 #define Rep(i,a,b) for(int i=(a);i<(b);i++)
 #define all(x) (x).begin(),(x).end()
 
-ll n;
-vector<ll> h(50050);
-
-ll dp[50050];
-
 int main() {
+  ll n;
   cin >> n;
-  rep(i,n) cin >> h[i];
-  dp[0] = 0;
-  dp[1] = abs(h[1]-h[0]);
-  Rep(i,2,n) {
-    dp[i] = min(dp[i-1]+abs(h[i]-h[i-1]),dp[i-2]+abs(h[i]-h[i-2]));
-  }
-  cout << dp[n-1]<< endl;
+  vector<ll> a(n);
+  rep(i,n) cin >> a[i];
+  vector<ll> dp(n+1,0);
+  dp[0]=0;
+  dp[1]=abs(a[1]-a[0]);
+  Rep(i,2,n) dp[i]=min(dp[i-1]+abs(a[i]-a[i-1]),dp[i-2]+abs(a[i]-a[i-2]));
+  cout << dp[n-1] << endl;
   return 0;
 }
