067a07573c99b20aec56e194c0f43a106de32148
SO0O0 <hirawatasou@gmail.com>
2023-03-24 16:00:34 +0900

    :gem: abcc

--- abc/abc279/c/main.cpp
+++ abc/abc279/c/main.cpp
@@ -1,30 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define Rep(i,a,b) for(int i=(a);i<(b);i++)
+#define all(x) (x).begin(),(x).end()
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int INF = 1e9;
+const ll LINF = 1e18;
 
-int main()
-{
-    int H, W;
-    cin >> H >> W;
-    string tmp;
-
-    vector<int> S(H), T(H);
-    for (int i = 0; i < H; i++)
-    {
-        cin >> tmp;
-        S[i] = count(tmp.cbegin(), tmp.cend(), '#');
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vector<string> s(h), t(h);
+  rep(i, h) cin >> s[i];
+  rep(i, h) cin >> t[i];
+  vector<string> s_col(w), t_col(w);
+  rep(i, w) {
+    rep(j, h) {
+      s_col[i] += s[j][i];
+      t_col[i] += t[j][i];
     }
-    for (int i = 0; i < H; i++)
-    {
-        cin >> tmp;
-        T[i] = count(tmp.cbegin(), tmp.cend(), '#');
+  }
+  sort(all(s_col));
+  sort(all(t_col));
+  rep(i, w) {
+    if (s_col[i] != t_col[i]) {
+      cout << "No" << endl;
+      return 0;
     }
-
-    if (S == T)
-    {
-        cout << "Yes" << endl;
-    }
-    else
-    {
-        cout << "No" << endl;
-    }
+  }
+  cout << "Yes" << endl;
+  return 0;
 }
